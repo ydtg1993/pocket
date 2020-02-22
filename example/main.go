a393fa7e6dac0ced22000d71c2a7b4731a06fb40
ydtg1993 <946818508@qq.com>
2020-02-22 14:35:53 +0800

    example

---
+++ example/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	pocket "github.com/ydtg1993/poket"
+	"net/http"
+	"time"
+)
+
+func main() {
+	p := &pocket.Pocket{
+		Connect:      32,
+		Port:         ":3001",
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	Api := pocket.Router{
+		Routes: map[string]http.HandlerFunc{
+			"/login": Login,
+		},
+		Middles: []pocket.MiddleHandler{
+			pocket.MethodMiddleHandler{Method: []string{pocket.METHOD_GET}},
+		},
+	}
+
+	p.Open([]pocket.Router{Api})
+}
+
+func Login(w http.ResponseWriter, req *http.Request) {
+	w.Write([]byte("开始注册!!"))
+}
